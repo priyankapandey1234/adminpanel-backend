cb2783ce7e59a61e4454863e9c6b68abb815e872
priyankapandey1234 <techzauvijek@gmail.com>
2024-12-16 11:03:54 +0530

    enquiry added

--- Router/enquiry.router.js
+++ Router/enquiry.router.js
@@ -1,5 +1,6 @@
 const express = require("express")
-const {createEnquiry,getAllEnquiries,updateEnquiryById} = require("../Controller/enquiry.controller")
+const {createEnquiry,getAllEnquiries,updateEnquiryById} = require("../Controller/enquiry.controller");
+const { authenticate } = require("../Controller/userController");
 
 // Create a new enquiry
 const router = express.Router();
@@ -7,7 +8,7 @@ const router = express.Router();
 router.post("/enquiries", createEnquiry);
 
 // Get all enquiries
-router.get("/enquiries", getAllEnquiries);
+router.get("/enquiries",authenticate, getAllEnquiries);
 
 // Update an enquiry by ID
 router.put("/enquiries/:id", updateEnquiryById);
